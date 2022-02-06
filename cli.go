672a523997dc57d9845097e8f0ea384dcf3d277a
theirish81 <theirish81@gmail.com>
2022-02-06 17:22:39 +0100

    removed unused colors. fixed number of stars in separator

--- cli.go
+++ cli.go
@@ -13,9 +13,7 @@ import (
 
 var (
 	colorReset  = "\033[0m"
-	colorRed    = "\033[31m"
 	colorYellow = "\033[33m"
-	colorGreen  = "\033[32m"
 )
 
 // printToCli will print the outcomes to CLI in the selected format
@@ -26,7 +24,7 @@ func printToCli(outcomes []Outcome, format string) {
 			tablePrintOutcomeToCLI(outcome)
 			if index < len(outcomes)-1 {
 				width, _, _ := term.GetSize(0)
-				for i := 0; i < width-13; i++ {
+				for i := 0; i < width-14; i++ {
 					fmt.Print("*")
 				}
 				fmt.Print("\n")
@@ -59,12 +57,23 @@ func buildTable(header ...string) *tablewriter.Table {
 	table.SetColMinWidth(1, width/2-10)
 	table.SetAutoWrapText(true)
 	table.SetColumnAlignment([]int{tablewriter.ALIGN_RIGHT, tablewriter.ALIGN_LEFT})
-	table.SetAutoWrapText(false)
+	table.SetAutoWrapText(true)
 	table.SetHeader(header)
 	table.SetColumnColor(tablewriter.Color(tablewriter.Normal, tablewriter.FgCyanColor),
 		tablewriter.Color(tablewriter.Normal, tablewriter.FgHiWhiteColor))
 	table.SetHeaderColor(tablewriter.Color(tablewriter.Bold, tablewriter.FgCyanColor), tablewriter.Color(tablewriter.Bold, tablewriter.FgCyanColor))
 	return table
+}
+
+func appendError(table *tablewriter.Table, label string, val string) {
+	table.Rich([]string{label, val}, []tablewriter.Colors{
+		{tablewriter.Normal, tablewriter.FgHiRedColor},
+		{}})
+}
+func appendSuccess(table *tablewriter.Table, label string, val string) {
+	table.Rich([]string{label, val}, []tablewriter.Colors{
+		{tablewriter.Normal, tablewriter.FgHiGreenColor},
+		{}})
 }
 
 func tablePrintOutcomeToCLI(outcome Outcome) {
@@ -78,8 +87,11 @@ func tablePrintOutcomeToCLI(outcome Outcome) {
 	fmt.Printf("%sResponse:%s\n", colorYellow, colorReset)
 	table = buildTable("Attribute", "Value")
 	table.Append([]string{"IP Address", outcome.IpAddress})
-	table.Append([]string{"Status", statusInColor(outcome)})
+	table.Append([]string{"Status", strconv.Itoa(outcome.Status)})
 	table.Append([]string{"Size", byteCountDecimal(outcome.Size)})
+	if outcome.Err != nil {
+		appendError(table, "Error", outcome.Err.Error())
+	}
 	table.Render()
 	table = buildTable("Metric", "Value")
 	fmt.Printf("%sMetrics:%s\n", colorYellow, colorReset)
@@ -104,14 +116,13 @@ func tablePrintOutcomeToCLI(outcome Outcome) {
 		fmt.Printf("%sAssertions:%s\n", colorYellow, colorReset)
 		table = buildTable("Assertion", "Result")
 		for _, check := range outcome.Checks {
-			color := colorRed
-			if check.Success {
-				color = colorGreen
-			}
 			output := fmt.Sprint(check.Output)
+			if check.Success {
+				appendSuccess(table, check.Assertion, output)
+			} else {
+				appendError(table, check.Assertion, output)
+			}
 
-			table.Append([]string{fmt.Sprintf("%s%s%s", color, check.Assertion, colorReset),
-				fmt.Sprintf("%s%s%s", color, output, colorReset)})
 		}
 		table.Render()
 	}
@@ -121,22 +132,8 @@ func tablePrintOutcomeToCLI(outcome Outcome) {
 func initColors() {
 	if runtime.GOOS == "windows" {
 		colorReset = ""
-		colorRed = ""
 		colorYellow = ""
-		colorGreen = ""
 	}
-}
-
-// statusInColor will print the status code in the right color
-func statusInColor(outcome Outcome) string {
-	if outcome.Status < 300 {
-		return colorGreen + strconv.Itoa(outcome.Status)
-	}
-	if outcome.Status < 400 {
-		return colorYellow + strconv.Itoa(outcome.Status)
-
-	}
-	return colorRed + strconv.Itoa(outcome.Status)
 }
 
 // byteCountDecimal will make the payload size human readable
